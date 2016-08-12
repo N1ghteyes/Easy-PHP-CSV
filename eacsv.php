91f0be83c55b0a72caa4b1451adcd1886b6ce794
Toby New <toby@maxx-design.co.uk>
2016-08-12 14:35:10 +0100

    Multiple Fixes
    Fixes including processing, output, headers, filename settings and a
    general tidy. Much to do however

--- eacsv.php
+++ eacsv.php
@@ -14,104 +14,127 @@
 class eacsv
 {
 
-  private $path; //path location for csv file pointer.
-  private $cp; //csv File pointer storage
-  private $deliminator; // Deliminator - not changeable after class is instantiated.
+    private $path; //path location for csv file pointer.
+    private $cp; //csv File pointer storage
+    private $deliminator; // Deliminator - not changeable after class is instantiated.
 
-  /**
-   * Constructor function - pass all info for the csv, none or somewhere in the middle.
-   * @param array $csvRowData
-   * @param $csvHeadData
-   * @param string $deliminator
-   * @param string $path
-   */
-  public function __construct($csvRowData = array(), $csvHeadData = array(), $deliminator = ',', $path = 'php://output'){
-    //set defaults
-    $this->deliminator = $this->_setDeliminator($deliminator);
-    $this->path = $path;
+    public $filename;
 
-    $this->cp = fopen($this->path, 'c+'); //open a new file, the pointer is automatically placed at the beginning.
-    fseek($this->cp, 0, SEEK_END ); // move the pointer to the end of the file opened.
+    /**
+     * Constructor function - pass all info for the csv, none or somewhere in the middle.
+     * @param array $csvRowData
+     * @param $csvHeadData
+     * @param string $deliminator
+     * @param string $path
+     */
+    public function __construct($csvRowData = array(), $csvHeadData = array(), $deliminator = ',', $filename = 'export.csv', $path = 'php://output')
+    {
+        //set defaults
+        $this->deliminator = $this->_setDeliminator($deliminator);
+        $this->path = $path;
+        $this->filename = $filename;
+        $this->_setExportHeaders();
 
-    if(!empty($csvHeadData)){ //assume we're adding a header regardless of file contents
-      $this->_processHeader($csvHeadData, TRUE);
+        $this->cp = fopen($this->path, 'c+'); //open a new file, the pointer is automatically placed at the beginning.
+        if($this->path != 'php://output') {
+            fseek($this->cp, 0, SEEK_END); // move the pointer to the end of the file opened.
+        }
+        if (!empty($csvHeadData)) { //assume we're adding a header regardless of file contents
+            $this->_processHeader($csvHeadData, TRUE);
+        }
+        if (!empty($csvRowData)) { //if we've passed in array data, add csv data to the file..
+            $this->_processArrays($csvRowData);
+        }
+
+        return $this;
     }
-    if(!empty($csvRowData)){ //if we've passed in array data, add csv data to the file..
-      $this->_processArrays($csvRowData);
+
+    /**
+     * allows for lazy / silly deliminators. - because why not.
+     * @param $deliminator
+     * @return string
+     */
+    private function _setDeliminator($deliminator)
+    {
+        switch ($deliminator) {
+            case 'comma':
+            case 'commer':
+                $delim = ',';
+                break;
+            case 'tab':
+            case '\t':
+                $delim = "\t";
+                break;
+            default:
+                $delim = $deliminator;
+        }
+        return $delim;
     }
 
-    return $this;
-  }
-
-  public function getCsv(){
-    if($this->path == 'php://output'){
-      fclose($this->cp); //we're outputting to browser so just close the pointer.
+    /**
+     * @param $csvHead
+     * @param bool|FALSE $start
+     * @todo - actually test this works..
+     */
+    private function _processHeader($csvHead, $start = FALSE)
+    {
+        if ($start) { //if start is true, add a line to the start of the csv. To do this, we need to create copy any existing data and recreate the file.
+            $pos = fgets($this->cp);
+            if ($pos != FALSE) { //if false, or 0 we can ignore this.
+                $initialfile = file_get_contents($this->path); //load the file contents
+                $this->cp = fopen($this->path, 'c+'); //open a new file.
+                fputcsv($this->cp, $csvHead); //write the header to the top of the new file
+                fwrite($this->cp, $initialfile); //add the old file onto the end of the header.
+            }
+        } else {
+            fputcsv($this->cp, $csvHead, $this->deliminator);
+        }
     }
-  }
 
-  public function saveCsv(){
-
-  }
-
-  /**
-   * @param $rows
-   */
-  public function addRows($rows){
-    $this->_processArrays($rows);
-    return $this;
-  }
-
-  /**
-   * @param $csvHead
-   * @param bool|FALSE $start
-   * @todo - actually test this works..
-   */
-  private function _processHeader($csvHead, $start = FALSE){
-    if($start){ //if start is true, add a line to the start of the csv. To do this, we need to create copy any existing data and recreate the file.
-      $pos = fgets($this->cp);
-      if($pos != FALSE){ //if false, or 0 we can ignore this.
-        $initialfile = file_get_contents($this->path); //load the file contents
-        $this->cp = fopen($this->path, 'c+'); //open a new file.
-        fputcsv($this->cp, $csvHead); //write the header to the top of the new file
-        fwrite($this->cp, $initialfile); //add the old file onto the end of the header.
-      }
-    } else {
-      fputcsv($this->cp, $csvHead, $this->deliminator);
+    /**
+     * @param $csvArray
+     * @return $this
+     */
+    private function _processArrays($csvArray)
+    {
+        $rows = count($csvArray); //count the rows, allows usage of for loops - much faster than foreach in this context.
+        $keys = array_keys($csvArray); //handle non numeric, non 0 arrays.
+        for ($i = 0; $i < $rows; ++$i) {
+            fputcsv($this->cp, $csvArray[$keys[$i]], $this->deliminator);
+        }
+        return $this;
     }
-  }
 
-  /**
-   * @param $csvArray
-   * @return $this
-   */
-  private function _processArrays($csvArray){
-    $rows = count($csvArray); //count the rows, allows usage of for loops - much faster than foreach in this context.
-    $keys = array_keys($csvArray); //handle non numeric, non 0 arrays.
-    for($i=0; $i < $rows; ++$i){
-      fputcsv($this->cp, $csvArray[$keys[$i]], $this->deliminator);
+    private function _setExportHeaders(){
+        if($this->path == 'php://output') {
+            header('Content-Type: application/csv');
+            header('Content-Disposition: attachment; filename=' . $this->filename . '.csv');
+            header('Pragma: no-cache');
+        }
     }
-    return $this;
-  }
 
-  /**
-   * allows for lazy / silly deliminators. - because why not.
-   * @param $deliminator
-   * @return string
-   */
-  private function _setDeliminator($deliminator){
-    switch($deliminator){
-      case 'comma':
-      case 'commer':
-        $delim = ',';
-        break;
-      case 'tab':
-      case '\t':
-        $delim = "\t";
-        break;
-      default:
-        $delim = $deliminator;
+    /**
+     * Function to handle the return of the CSV file.
+     * @return $this
+     */
+    public function getCsv()
+    {
+        fclose($this->cp); //we're outputting to browser so just close the pointer.
+        return $this;
     }
-    return $delim;
-  }
+
+    public function saveCsv()
+    {
+
+    }
+
+    /**
+     * @param $rows
+     */
+    public function addRows($rows)
+    {
+        $this->_processArrays($rows);
+        return $this;
+    }
 
 }
